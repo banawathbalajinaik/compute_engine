8c4b6c9aa8515a38d75a5b2c8aaeb42de729009f
banawathbalajinaik <83709183+banawathbalajinaik@users.noreply.github.com>
2022-05-05 14:32:59 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,13 +1,13 @@
 provider "google" {
   project     = "tesing-342306"
-  region      = "us-central1"
+  region      = "asia-south1"
   credentials = file("tesing-342306-f93e1b4153bc.json")
 }
 
 resource "google_compute_instance" "balaji" {
   name         = "testing1"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  zone         = "asia-south11-a"
 
   boot_disk {
     initialize_params {
