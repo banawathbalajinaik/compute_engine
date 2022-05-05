cae5f6b44f1c34faee98771ed7e11173916ad432
banawathbalajinaik <83709183+banawathbalajinaik@users.noreply.github.com>
2022-05-05 14:28:51 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -7,17 +7,12 @@ provider "google" {
 resource "google_compute_instance" "balaji" {
   name         = "testing1"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  zone         = "us-central1"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
   }
 
   network_interface {
